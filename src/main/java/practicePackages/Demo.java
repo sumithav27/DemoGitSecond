a11955215debb82b0842447a38b4531b857b48e2
sumithav27 <sumi2702@gmail.com>
2022-08-25 06:51:58 +0530

    second commit by X

---
+++ src/main/java/practicePackages/Demo.java
@@ -0,0 +1,10 @@
+package practicePackages;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("main class in main java");
+
+	}
+
+}
--- src/test/java/practiceTestPackage/Day1Test.java
+++ src/test/java/practiceTestPackage/Day1Test.java
@@ -1,4 +1,4 @@
-package UdemyProject.PractiseMaven;
+package practiceTestPackage;
 
 import org.testng.annotations.Test;
 
